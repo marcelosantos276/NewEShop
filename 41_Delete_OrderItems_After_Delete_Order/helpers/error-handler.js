593ca1457e00bbe078736bf78d53afc86ad42e9a
=marcelo.santos276 <=marcelo.santos276@etec.sp.gov.br>
2023-05-04 20:42:02 -0300

    Nova

---
+++ 41_Delete_OrderItems_After_Delete_Order/helpers/error-handler.js
@@ -0,0 +1,10 @@
+module.exports = function errorHandler(err, req, res, next) {
+    if(err.name === 'UnauthorizedError'){
+       return res.status(400).json({message: 'The user not authorized'});
+    }
+    if(err.name === 'ValidationError') {
+        return res.status(401).json({message: err});
+    }
+
+    return res.status(500).json({message: err});
+}
